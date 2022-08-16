eaa5c5af1f42d608fc97174629d7da70a9d6d317
DANIEL FRANCISCO ACUNA MENDEZ <danielfrancismendez@gmail.com>
2022-08-16 18:33:11 -0500

    Corrección1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -62,14 +62,9 @@ namespace daniel.Controllers
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
+
 
 
 
