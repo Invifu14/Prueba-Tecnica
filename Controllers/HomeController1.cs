a9a9122bebb9530677e48409c429e741a539f100
DANIEL FRANCISCO ACUNA MENDEZ <danielfrancismendez@gmail.com>
2022-08-18 15:10:35 -0500

    Correccion, comentar codigo consumo api

--- Controllers/HomeController1.cs
+++ Controllers/HomeController1.cs
@@ -12,29 +12,29 @@ namespace daniel.Controllers
     {
         //Hosted web API REST Service base url
         string Baseurl = "";
-        public async Task<ActionResult> Index()
-        {
-            List<PAIS> EmpInfo = new List<PAIS>();
-            using (var client = new HttpClient())
-            {
-                //Passing service base url
-                client.BaseAddress = new Uri(Baseurl);
-                client.DefaultRequestHeaders.Clear();
-                //Define request data format
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                //Sending request to find web api REST service resource GetAllEmployees using HttpClient
-                HttpResponseMessage Res = await client.GetAsync("api/Employee/GetAllEmployees");
-                //Checking the response is successful or not which is sent using HttpClient
-                if (Res.IsSuccessStatusCode)
-                {
-                    //Storing the response details recieved from web api
-                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
-                    //Deserializing the response recieved from web api and storing into the Employee list
-                    EmpInfo = JsonConvert.DeserializeObject<List<PAIS>>(EmpResponse);
-                }
-                //returning the list to view
-                return View(EmpInfo);
-            }
-        }
+        //public async Task<ActionResult> Index()
+        //{
+        //    List<PAIS> EmpInfo = new List<PAIS>();
+        //    using (var client = new HttpClient())
+        //    {
+        //        //Passing service base url
+        //        client.BaseAddress = new Uri(Baseurl);
+        //        client.DefaultRequestHeaders.Clear();
+        //        //Define request data format
+        //        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        //        //Sending request to find web api REST service resource GetAllEmployees using HttpClient
+        //        HttpResponseMessage Res = await client.GetAsync("api/Employee/GetAllEmployees");
+        //        //Checking the response is successful or not which is sent using HttpClient
+        //        if (Res.IsSuccessStatusCode)
+        //        {
+        //            //Storing the response details recieved from web api
+        //            var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+        //            //Deserializing the response recieved from web api and storing into the Employee list
+        //            EmpInfo = JsonConvert.DeserializeObject<List<PAIS>>(EmpResponse);
+        //        }
+        //        //returning the list to view
+        //        return View(EmpInfo);
+        //    }
+        //}
     }
 }
